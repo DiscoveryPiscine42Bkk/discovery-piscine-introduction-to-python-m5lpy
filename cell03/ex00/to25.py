4dab2bb613fc9b1f293edde653d9aad829621169
m5lpy <nudthaundh@gmail.com>
2025-06-22 18:35:25 +0000

    commit 3.0

---
+++ cell03/ex00/to25.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+print("Enter a number less than 25")
+user_input_num = int(input())
+# variable = input("prompt")
+
+if user_input_num < 25 :
+    count = user_input_num
+    while count <= 25:
+        print(f"Inside the loop, my variable is {count}")           # f-string (f...) is for printing variables nicely, whatever inside {} will be repeated until the loop ends
+else:
+    print("Error")
