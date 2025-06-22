cd0735f66eddb816d2e9b80008344e2b18b168e7
m5lpy <nudthaundh@gmail.com>
2025-06-22 18:32:31 +0000

    commit 1.3 (revised)

--- cell01/ex03/whatsyourname.py
+++ cell01/ex03/whatsyourname.py
@@ -1,5 +1,6 @@
+#!/usr/bin/python3
 print("Hey, what is your first name? :")
 first_name = input()
 print("And your last name? :")
 last_name = input()
-print("Well, pleased to meet you,", first_name, last_name)
+print("Well, pleased to meet you,", first_name, last_name +".")
