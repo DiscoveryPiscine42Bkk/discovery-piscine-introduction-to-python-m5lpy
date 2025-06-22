51a70d7b9867708dcd70a74366d7fbfe9913666e
m5lpy <nudthaundh@gmail.com>
2025-06-22 03:08:55 +0000

    commit 2.1

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+number = int(input())
+
+if number == 0:
+    print("This number is positive and negative")
+elif number > 0:
+    print("This number is positive")
+else:
+    print("This number is negative")
+
+#one equal sign '=' is assignment
+#'==' is comparison
+#1 = 1 # error
+#1 == 1 # true
+# 1 = 2 # false
