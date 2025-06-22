0c80ac2b2122d31f316f82a670ba8a1a9a1bbb33
m5lpy <nudthaundh@gmail.com>
2025-06-22 18:33:12 +0000

    commit 2.0 (revised)

--- cell02/ex00/iszero.py
+++ cell02/ex00/iszero.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-number = int(input())
+number = int(input())     # int means convert something into number if possible
 if int(number) == 0:
     print("This number is equal to zero.")
 else:
