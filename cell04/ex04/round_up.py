5e1794caea6dbd99842ca177b72122f6fbf35198
m5lpy <nudthaundh@gmail.com>
2025-06-22 18:38:16 +0000

    commit 4.4

---
+++ cell04/ex04/round_up.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+import math         # import pulls the function out of math to use
+number = input("Give me a number: ")
+print(math.ceil(float(number)))         # ciel rounds up the decimals (in this case the integer math), float() converts a value into a decimal number
