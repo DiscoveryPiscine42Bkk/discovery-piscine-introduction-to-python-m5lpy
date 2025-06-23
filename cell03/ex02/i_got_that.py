ab0f1bb426e2cb3488a45c67f21904a61c4cd6b9
m5lpy <nudthaundh@gmail.com>
2025-06-23 01:56:28 +0000

    commit 3.2 (V.2)

--- cell03/ex02/i_got_that.py
+++ cell03/ex02/i_got_that.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-input("What you gotta say? : ")
+answer = input("What you gotta say? : ")
 while True:       # true means that the loop is true so it keeps looping
-    answer = input("I got that! Anything else? : ")
     if answer == "STOP":
         break
+    answer = input("I got that! Anything else? : ")
