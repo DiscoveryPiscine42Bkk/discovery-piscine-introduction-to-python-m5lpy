7e3c0fcd7bbda85e9013204f959b893c91006bdf
m5lpy <nudthaundh@gmail.com>
2025-06-23 01:48:33 +0000

    FixCell03Ex00

--- cell03/ex00/to25.py
+++ cell03/ex00/to25.py
@@ -7,5 +7,6 @@ if user_input_num < 25 :
     count = user_input_num
     while count <= 25:
         print(f"Inside the loop, my variable is {count}")           # f-string (f...) is for printing variables nicely, whatever inside {} will be repeated until the loop ends
+        count += 1
 else:
     print("Error")
